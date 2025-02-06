12afb616aff17226ef7cd5f79a7e4ea4bc96ebf5
Roman Chadnov <125334511+rchadnov@users.noreply.github.com>
2025-02-06 11:35:12 -0800

    feat: add images navigation button to sidebar

--- src/lib/components/layout/Sidebar.svelte
+++ src/lib/components/layout/Sidebar.svelte
@@ -3,6 +3,7 @@
 	import { v4 as uuidv4 } from 'uuid';
 
 	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
 	import {
 		user,
 		chats,
@@ -549,7 +550,6 @@
 							showSidebar.set(false);
 						}
 					}}
-					draggable="false"
 				>
 					<div class="self-center">
 						<svg
@@ -574,6 +574,39 @@
 				</a>
 			</div>
 		{/if}
+
+		<div class="px-1.5 flex flex-col gap-1">
+			<div class="flex justify-center text-gray-800 dark:text-gray-200">
+				<a
+					class="flex-grow flex space-x-3 rounded-lg px-2 py-[7px] hover:bg-gray-100 dark:hover:bg-gray-900 transition {$page.url.pathname === '/images' ? 'bg-gray-100 dark:bg-gray-900' : ''}"
+					href="/images"
+					on:click={() => {
+						goto('/images');
+						if ($mobile) {
+							showSidebar.set(false);
+						}
+					}}
+				>
+					<div class="self-center">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="2"
+							stroke="currentColor"
+							class="size-[1.1rem]"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M13.5 16.875h3.375m0 0h3.375m-3.375 0V13.5m0 3.375v3.375M6 10.5h2.25a2.25 2.25 0 0 0 2.25-2.25V6a2.25 2.25 0 0 0-2.25-2.25H6A2.25 2.25 0 0 0 3.75 6v2.25A2.25 2.25 0 0 0 6 10.5Zm0 9.75h2.25A2.25 2.25 0 0 0 10.5 18v-2.25a2.25 2.25 0 0 0-2.25-2.25H6a2.25 2.25 0 0 0-2.25 2.25V18A2.25 2.25 0 0 0 6 20.25Zm9.75-9.75H18a2.25 2.25 0 0 0 2.25-2.25V6A2.25 2.25 0 0 0 18 3.75h-2.25A2.25 2.25 0 0 0 13.5 6v2.25a2.25 2.25 0 0 0 2.25 2.25Z"
+							/>
+						</svg>
+					</div>
+					<div class="flex-1 text-left truncate">Images</div>
+				</a>
+			</div>
+		</div>
 
 		<div class="relative {$temporaryChatEnabled ? 'opacity-20' : ''}">
 			{#if $temporaryChatEnabled}
